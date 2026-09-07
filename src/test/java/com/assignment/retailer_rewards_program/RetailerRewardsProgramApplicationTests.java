e95fa02165b040f9a7d8a1c949e535764283a2a7
Anurag Ratna <anuragratna00@gmail.com>
2026-09-07 15:29:06 +0530

    Initialize Spring Boot application

---
+++ src/test/java/com/assignment/retailer_rewards_program/RetailerRewardsProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.retailer_rewards_program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailerRewardsProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
